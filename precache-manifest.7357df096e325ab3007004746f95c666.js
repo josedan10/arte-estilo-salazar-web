d0028d1f670d8f51c351b9b3e9b9b6627e5374e2
0x6A6F736564616E3130 <josedan10@users.noreply.github.com>
2020-02-06 17:51:47 -0400

    Updates

---
+++ precache-manifest.7357df096e325ab3007004746f95c666.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b9bff0d8f9365272f404e8faeaf682",
+    "url": "/arte-estilo-salazar-web/index.html"
+  },
+  {
+    "revision": "344ab51cedc5bb8760f7",
+    "url": "/arte-estilo-salazar-web/static/css/2.79a28be8.chunk.css"
+  },
+  {
+    "revision": "c0a67598cc6b2cea2196",
+    "url": "/arte-estilo-salazar-web/static/css/main.3dad6475.chunk.css"
+  },
+  {
+    "revision": "344ab51cedc5bb8760f7",
+    "url": "/arte-estilo-salazar-web/static/js/2.511ec4cb.chunk.js"
+  },
+  {
+    "revision": "c0a67598cc6b2cea2196",
+    "url": "/arte-estilo-salazar-web/static/js/main.a0cd77c9.chunk.js"
+  },
+  {
+    "revision": "214d5d06897124835897",
+    "url": "/arte-estilo-salazar-web/static/js/runtime-main.63be2096.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/arte-estilo-salazar-web/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/arte-estilo-salazar-web/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/arte-estilo-salazar-web/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/arte-estilo-salazar-web/static/media/slick.f97e3bbf.svg"
+  }
+]);
